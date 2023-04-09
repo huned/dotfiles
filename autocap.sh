c837864a60ab022823d2fa8c821561d355ca69e5
Huned Botee <huned@hunedbotee.com>
2023-04-09 16:36:14 -0400

    Autocap doesn't save dups.

--- autocap.sh
+++ autocap.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# make sure you have: xprintidle, imagemagick
+# Dependencies: xprintidle, imagemagick, sha256sum
 
-SAVE_DIR="$HOME/Pictures/Screenshots/autocap"
+SAVE_DIR="${HOME}/Pictures/Screenshots/autocap"
+HASH_DIR="${SAVE_DIR}/.hashes"
+
 mkdir -p "${SAVE_DIR}"
+mkdir -p "${HASH_DIR}"
 
 # The main loop that takes screenshots every 10 minutes
 while true
@@ -14,8 +17,20 @@ do
     filename="${SAVE_DIR}/autocap-$(date +%F-%T).png"
     import -window root $filename
 
-    # Delete all images that are older than one year
-    find "${SAVE_DIR}" -type f -mtime +365 -delete
+    # Calculate the hash of the file contents using sha256sum
+    file_hash=$(sha256sum "$filename" | awk '{ print $1 }')
+
+    # Check if a file with the same hash already exists
+    if [ -f "${HASH_DIR}/${file_hash}" ]; then
+      # Remove the duplicate file
+      rm "$filename"
+    else
+      # Create a file with the hash as the filename to mark it as a duplicate
+      touch "${HASH_DIR}/${file_hash}"
+    fi
+
+    # Delete all images that are older than five years
+    find "${SAVE_DIR}" -type f -mtime +1825 -delete
   fi
 
   # Wait for 10 minutes before taking another screenshot
