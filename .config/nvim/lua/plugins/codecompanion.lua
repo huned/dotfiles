4e9302af050547e426ef932e95ef408215ddfab9
Huned Botee <huned@hunedbotee.com>
2025-08-09 12:49:26 -0500

    update codecompanion neovim plugin settings

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -9,6 +9,25 @@ require("codecompanion").setup({
   ---   }
   --- },
   adapters = {
+    opts = {
+      show_defaults = false,
+    },
+    ollama = function ()
+      return require("codecompanion.adapters").extend("ollama", {
+          name = "qwen3",
+          schema = {
+            model = {
+              default = "qwen3:4b",
+            },
+            num_ctx = {
+              default = 16384,
+            },
+            num_predict = {
+              default = -1,
+            }
+          },
+        })
+    end,
     openai = function()
       return require("codecompanion.adapters").extend("openai", {
           env = {
@@ -20,7 +39,8 @@ require("codecompanion").setup({
       return require("codecompanion.adapters").extend("anthropic", {
           schema = {
             model = {
-              default = "claude-3-5-sonnet-20241022",
+              -- default = "claude-3-5-sonnet-20241022",
+              default = "claude-3-7-sonnet-20250219",
             },
           },
         })
