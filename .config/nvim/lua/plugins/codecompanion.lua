515a867632538c1f1ead93f04d7929b58f9dfb76
Huned Botee <huned@hunedbotee.com>
2025-10-28 16:15:16 -0500

    Update CodeCompanion settings -- maintainer changed the config format.

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -9,43 +9,46 @@ require("codecompanion").setup({
   ---   }
   --- },
   adapters = {
-    opts = {
-      show_defaults = false,
-    },
-    ollama = function ()
-      return require("codecompanion.adapters").extend("ollama", {
-          name = "qwen3",
-          schema = {
-            model = {
-              default = "qwen3:4b",
+    http = {
+      opts = {
+        show_defaults = false,
+      },
+      ollama = function ()
+        return require("codecompanion.adapters").extend("ollama", {
+            name = "qwen3",
+            schema = {
+              model = {
+                default = "qwen3:4b",
+              },
+              num_ctx = {
+                default = 16384,
+              },
+              num_predict = {
+                default = -1,
+              }
             },
-            num_ctx = {
-              default = 16384,
+          })
+      end,
+      openai = function()
+        return require("codecompanion.adapters").extend("openai", {
+            env = {
+              api_key = "OPENAI_API_KEY",
             },
-            num_predict = {
-              default = -1,
-            }
-          },
-        })
-    end,
-    openai = function()
-      return require("codecompanion.adapters").extend("openai", {
-          env = {
-            api_key = "OPENAI_API_KEY",
-          },
-        })
-    end,
-    anthropic = function()
-      return require("codecompanion.adapters").extend("anthropic", {
-          schema = {
-            model = {
-              -- default = "claude-3-5-sonnet-20241022",
-              -- default = "claude-3-7-sonnet-20250219",
-              default = "claude-4-sonnet-20250514",
+          })
+      end,
+      anthropic = function()
+        return require("codecompanion.adapters").extend("anthropic", {
+            schema = {
+              model = {
+                -- default = "claude-3-5-sonnet-20241022",
+                -- default = "claude-3-7-sonnet-20250219",
+                -- default = "claude-4-sonnet-20250514",
+                default = "claude-haiku-4-5-20251001",
+              },
             },
-          },
-        })
-    end,
+          })
+      end,
+    }
   },
   strategies = {
     chat = {
