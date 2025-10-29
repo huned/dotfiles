b78316b8c78b3b3ae2592bbebe5d795b46681fd6
Huned Botee <huned@hunedbotee.com>
2025-10-28 20:08:24 -0500

    Remove unused stuff.

--- install-scripts/install-beekeeper.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-wget --quiet -O - https://deb.beekeeperstudio.io/beekeeper.key | apt-key add -
-echo "deb https://deb.beekeeperstudio.io stable main" > /etc/apt/sources.list.d/beekeeper-studio-app.list
-apt-get -y update && apt-get -y install beekeeper-studio
